6bd97ba77cc7f3db162952038a57a50906d26c4e
devsamin <abdullahalsamin79@gmail.com>
2025-07-10 10:58:53 -0700

    set up public way

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -167,8 +167,11 @@ USE_TZ = True
 import os
 from pathlib import Path
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'public/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
