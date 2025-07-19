365baca766dc31a35479bb5c5ae1863888a696f9
devsamin <abdullahalsamin79@gmail.com>
2025-07-19 10:41:15 -0700

    deploy problems

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# make migrations and migrate the database
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-# collect static files
-python3 manage.py collectstatic --noinput --clear
