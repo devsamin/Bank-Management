029dfb9f510f1b01104035ff2572277496fb2c8f
devsamin <abdullahalsamin79@gmail.com>
2025-07-19 10:33:13 -0700

    deploy problem

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECRET_KEY = 'django-insecure-ysvf#-3&t^5-rg34ip%)&zw5okhkctsf0(@y@_g1p_v3pk0j2*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["*"]
 # CSRF_TRUSTED_ORIGINS = ['https://bank-management-r8c6.onrender.com','https://*.127.0.0.1']
@@ -171,8 +171,8 @@ from pathlib import Path
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # এটা যুক্ত করো!
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
