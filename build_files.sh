75a2545dafb709115e4451534a78a4ddece2ab02
devsamin <abdullahalsamin79@gmail.com>
2025-08-03 23:48:33 -0700

    update error  deploy

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 
-# build_files.sh
+echo "Installing dependencies..."
+python3 -m pip install -r requirements.txt || exit 1
 
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput || exit 1
+
+echo "Build success!"
