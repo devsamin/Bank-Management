c3fb693bc425a39d53854646ffbc215b181e0a72
devsamin <abdullahalsamin79@gmail.com>
2025-08-03 22:53:00 -0700

    update error for deploy

--- api/index.py
+++
@@ -1,5 +0,0 @@
-from mamar_bank.wsgi import application
-from vercel_wsgi import handle_request
-
-def handler(environ, start_response):
-    return handle_request(application, environ, start_response)
