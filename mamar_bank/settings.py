1374aba74d60644d812741637c413b8bb9ed0056
devsamin <abdullahalsamin79@gmail.com>
2025-08-04 00:35:57 -0700

    update without bulid.sh  deploy2

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 # ALLOWED_HOSTS = ["*"]
 # CSRF_TRUSTED_ORIGINS = ['https://bank-management-r8c6.onrender.com','https://*.127.0.0.1']
 
-ALLOWED_HOSTS = [".vercel.app","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app","127.0.0.1",'.now.sh']
 # ALLOWED_HOSTS = [
 #     "127.0.0.1",
 #     "localhost",
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -171,6 +172,7 @@ from pathlib import Path
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATIC_URL = '/static/'
 # # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
