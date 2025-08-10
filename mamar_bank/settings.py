647c2f3226b2aea2902ceeee55814671bad15139
devsamin <abdullahalsamin79@gmail.com>
2025-08-10 04:19:24 -0700

    CSRF link added

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://bank-management-r8c6.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://trust-bank.onrender.com','https://*.127.0.0.1']
 
 # ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 # ALLOWED_HOSTS = [
